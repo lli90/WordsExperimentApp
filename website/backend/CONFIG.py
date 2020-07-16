5f069f90140dcfa12938276cb9d34c59b49e758c
AidanFray <afray@protonmail.com>
2020-07-16 10:18:20 +0100

    Added missing backend files

---
+++ website/backend/CONFIG.py
@@ -0,0 +1,11 @@
+BASE_FILE_LOCATION = "" #B700
+ATTACK_CHANCE = 0.3
+NUMBER_OF_ROUNDS = 35
+GRACE_ROUNDS = 5 # Number of initial rounds that can't contain an attack
+
+## NOTE:
+#   Naming convension for files is:
+#
+#       ./data/similar/<lower_case_name>.csv
+#
+ATTACK_METRICS = ['LEVEN', "METAPHONE", "NYSIIS"]
